3238ba3df424f05d74bb6f6dfc8a4e26ab09a424
Jo Wagner <jowagner@limago.de>
2021-09-12 10:44:49 +0200

    initial commit

---
+++ WebGridDemo/WebGridDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+/*
+ * ##############################################
+ *
+ *  https://aspnet-core-grid.azurewebsites.net/installation Webseite mit Anleitung. Es gibt aber auch viele Alternativen
+ *
+ *##############################################
+ *
+ *
+ */
+
+
+namespace WebGridDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IQueryable<Person> People { get; set; }
+
+        public void OnGet()
+        {
+            People = GetPeople().AsQueryable();
+        }
+
+        private List<Person> GetPeople()
+        {return new List<Person> { 
+                 new Person ("Max", "Mustermann", "Single", 18, DateTime.Now,  true),
+                 new Person ("Max", "Mustermann", "Single", 18, DateTime.Now,  true),
+                 new Person ("Max", "Mustermann", "Single", 18, DateTime.Now,  true),
+                 new Person ("Max", "Mustermann", "Single", 18, DateTime.Now,  true)
+
+            };
+            
+        }
+    }
+}
---
+++ WebGridDemo/WebGridDemo/Pages/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGridDemo.Pages
+{
+    public class Person
+    {
+       
+
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Status { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Employed { get; set; }
+
+        public Person(string name, string surname, string status, int age, DateTime birthday, bool employed)
+        {
+            Name = name;
+            Surname = surname;
+            Status = status;
+            Age = age;
+            Birthday = birthday;
+            Employed = employed;
+        }
+
+    }
+}
